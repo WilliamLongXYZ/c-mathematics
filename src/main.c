e48af4563c9f77f3963a272ab5a4ffa2f200b009
William Long <will@williamlong.xyz>
2021-07-09 04:36:37 +0000

    Imported mathematical functions from a project which I had previously lost, and was a replica of this, yet missing the Git repository.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <math.h>
+
+int x;
+int y;
+
+int add(int x, int y) {
+    return x + y;
+}
+
+int sub(int x, int y) {
+    return x - y;
+}
+
+int mul(int x, int y) {
+    return x * y;
+}
 
 int main(void) {
-  printf("Hello World\n");
+  while (1) {
+      scanf("%d %d", &x, &y);
+  }
   return 0;
 }
